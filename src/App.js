0c409a5e89a454197cc84d423f8a3c97fb7847a0
Sriyazhin <128023853+Sriyazhin@users.noreply.github.com>
2023-07-27 22:19:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import './App.css';
-import Cards from './commonCard';
+import Cards from './commoncard';
+import Notes from './assets/images/notes.jpg';
+import Books from './assets/images/books.jpg';
+import Laptop from './assets/images/laptop.jpg';
 function App() {
-  
+  const data=[
+    {imageName:Notes,
+      titleName:'General English'
+    },
+    {imageName:Books,
+      titleName:'Classic English'},
+    {imageName:Laptop,
+      titleName:'Business English'}];
   return (
     <div className='full'>
 
-      <div className='first'>
+      <div className='fortitle'>
       <div className='lineone'>Find a Course</div>
       <div className='linetwo'>Featured Courses</div>
       <div><hr className='linethree'></hr></div>
       </div>
 
-      <div className='second'>
-        <Cards imagename={require('./assets/images/notes.jpg') } titlename='General English' NameinButton='MORE' />
-        <Cards imagename={require('./assets/images/books.jpg') } titlename='Classic English' NameinButton='MORE' />
-        <Cards imagename={require('./assets/images/laptop.jpg') } titlename='Business English' NameinButton='MORE' />
+      <div className='forcards'>
+      {data.map((details, index) => {
+        return (
+          <Cards key={index} imagename={details.imageName} titlename={details.title} nameinbutton='MORE' />
+        );
+      })}
+        
       </div>
 
-      <div className='third'>
+      <div>
 <div className='linefour'>Images from <b>Freepik</b></div>
 <div><hr className='lastline'></hr></div>
       </div>
+
     </div>
   );
 }
